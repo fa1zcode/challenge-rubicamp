d55e6f6cce98e5310fa62ec35e340bc95a323c59
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-14 19:05:19 +0700

    only print words started with vowels

--- test1.js
+++ test1.js
@@ -1,8 +1,9 @@
 const vowel = ('AEIOUaeiou')
 let sentence = 'ibu pergi ke pasar bersama aku'
 let kalimat = sentence.split(' ')
-//console.log(kalimat[0][0])
+
 
 for (i=0 ; i<kalimat.length ; i++){
-    console.log(kalimat[i][0])
+    if (vowel.match((kalimat[i][0]))) {console.log(kalimat[i])}
+    else console.log('try again')
 }
