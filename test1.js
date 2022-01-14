9e5f2e97be27333904784c8c9887c667939ec52e
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-14 19:17:42 +0700

    ready for chlg 6

--- test1.js
+++ test1.js
@@ -5,5 +5,5 @@ let kalimat = sentence.split(' ')
 
 for (i=0 ; i<kalimat.length ; i++){
     if (vowel.match((kalimat[i][0]))) {console.log(kalimat[i])}
-    else console.log('try again')
+    else console.log(kalimat[i].slice(1,kalimat[i].length) + kalimat[i][0] + 'nyo')
 }
