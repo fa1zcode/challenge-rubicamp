8ec184e987f2cc9cd015be44f8381769510a5c2b
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-15 13:34:36 +0700

    array to string sentence

--- test1.js
+++ test1.js
@@ -1,9 +1,15 @@
-const vowel = ('AEIOUaeiou')
-let sentence = 'ibu pergi ke pasar bersama aku'
-let kalimat = sentence.split(' ')
+function stringManipulation(sentence) {
+    const vowel = ('AEIOUaeiou')
+    let kalimat = sentence.split(' ')
+    let manipulated = []
 
 
-for (i=0 ; i<kalimat.length ; i++){
-    if (vowel.match((kalimat[i][0]))) {console.log(kalimat[i])}
-    else console.log(kalimat[i].slice(1,kalimat[i].length) + kalimat[i][0] + 'nyo')
+    for (i = 0; i < kalimat.length; i++) {
+        if (vowel.match((kalimat[i][0]))) { manipulated.push(kalimat[i]) }
+        else manipulated.push(kalimat[i].slice(1, kalimat[i].length) + kalimat[i][0] + 'nyo')
+ 
+    } console.log(manipulated.join(' '))
 }
+
+
+stringManipulation('ibu pergi ke pasar bersama aku')
