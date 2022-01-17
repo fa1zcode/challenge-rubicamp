9e200fc1c6d9bd9291767e5e5e68679dc8538e7d
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-17 10:51:31 +0700

    challenge 7 done

--- 7weirdmultiplychlnge.js
+++ 7weirdmultiplychlnge.js
@@ -1,37 +1,17 @@
-/*function weirdMultiply (sentence){
+function weirdMultiply (sentence){
 
-    let str = Array.from(sentence.toString())
+    let str = sentence.toString()
     let sum = 1
 
     for (i=0 ; i<str.length ; i++){
         sum = sum * str[i]
     } if (sum < 10){
         return sum
-    } else weirdMultiply(sum)
+    } else return weirdMultiply(sum)
 
 }
 console.log(weirdMultiply(39))
 console.log(weirdMultiply(999))
 console.log(weirdMultiply(3))
-*/
+console.log(weirdMultiply(1234))
 
-
-
-function multiply(num) {
-
-    let arr = num.toString()
-    let sum = 1
-    
-
-    for (i = 0; i < arr.length; i++) {
-        sum = sum * arr[i]
-    } if (sum < 10) {
-        console.log(sum)
-    }
-    else multiply(sum)
-
-}
-
-multiply(39)
-multiply(999)
-multiply(3)
