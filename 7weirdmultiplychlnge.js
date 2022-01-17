91f4dbdd23aa83b76437b6cd0fdeddafe2b2226c
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-17 09:55:41 +0700

    7 done but console.log still inside function

--- 7weirdmultiplychlnge.js
+++ 7weirdmultiplychlnge.js
@@ -1,5 +1,14 @@
 /*function weirdMultiply (sentence){
 
+    let str = Array.from(sentence.toString())
+    let sum = 1
+
+    for (i=0 ; i<str.length ; i++){
+        sum = sum * str[i]
+    } if (sum < 10){
+        return sum
+    } else weirdMultiply(sum)
+
 }
 console.log(weirdMultiply(39))
 console.log(weirdMultiply(999))
@@ -10,18 +19,19 @@ console.log(weirdMultiply(3))
 
 function multiply(num) {
 
-    let arr = Array.from(num.toString())
+    let arr = num.toString()
     let sum = 1
-    let lastMultiply = []
+    
 
     for (i = 0; i < arr.length; i++) {
         sum = sum * arr[i]
     } if (sum < 10) {
         console.log(sum)
     }
-    else console.log('repeat')
-
+    else multiply(sum)
 
 }
 
 multiply(39)
+multiply(999)
+multiply(3)
