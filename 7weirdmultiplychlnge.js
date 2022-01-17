42825ddec36e02110f1e19d0b93aa189653d393f
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-17 08:55:17 +0700

    only print if single digit

--- 7weirdmultiplychlnge.js
+++ 7weirdmultiplychlnge.js
@@ -10,12 +10,18 @@ console.log(weirdMultiply(3))
 
 function multiply(num) {
 
-    let str = Array.from(num.toString())
+    let arr = Array.from(num.toString())
     let sum = 1
+    let lastMultiply = []
 
-    for (i = 0; i < str.length; i++) {
-        sum = sum * str[i]
-    } return sum
+    for (i = 0; i < arr.length; i++) {
+        sum = sum * arr[i]
+    } if (sum < 10) {
+        console.log(sum)
+    }
+    else console.log('repeat')
+
+
 }
 
-console.log(multiply(223))
+multiply(39)
