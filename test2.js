c350b5705623b34b47491c9dd0d540aa43e63635
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-19 12:03:48 +0700

    challenge 8 done

--- test2.js
+++ test2.js
@@ -5,10 +5,14 @@ let jumlah = []
 for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
         let arr2 = arr[4].replace('#',j)
-        console.log(arr2)
+        let arr1 = arr[0].replace('#',i)
+        if (arr1 * arr[2] == arr2) {
+            console.log([i,j]) 
+        }
+        
     } 
-    let arr1 = arr[0].replace('#',i)
-    console.log(arr1)
+    
+    
     
 }
 
