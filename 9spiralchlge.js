da18a408c5c2b29382e17478850b44dc0d4c894e
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-20 21:19:08 +0700

    print first row

--- 9spiralchlge.js
+++ 9spiralchlge.js
@@ -1,9 +1,12 @@
 function spiral (param1){
-
     let arr = []
-    for (i=0 ; i<param1*param1 ; i++){
+    
+
+    for(i=0 ;i<param1 ; i++){ //first row
         arr.push(i)
-    }  console.log(arr)
+    } console.log(arr)
+
+
 }
 
 spiral(5)
