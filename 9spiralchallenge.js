4b0ac8503c157bf963883f0e83e6d823e155e357
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-22 19:20:38 +0700

    print top right bottom left

--- 9spiralchallenge.js
+++ 9spiralchallenge.js
@@ -1,41 +1,53 @@
-function spiral (param1){
+function spiral(param1) {
 
-    let matrix = []
-    let count = 0
+  let matrix = []
+  let count = 0
 
-    for (let i = 0; i < param1; i++) {
-        matrix[i] = [];
-        for (let j = 0; j < param1; j++) {
-          matrix[i][j] = count;
-          count++;
-        }
-      } console.log(matrix)
-    
-    let arr = []
-    let x = 0 // index array
-    let y = 0 // index element
-    let top = param1
-    let bottom = 0
+  for (let i = 0; i < param1; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < param1; j++) {
+      matrix[i][j] = count;
+      count++;
+    }
+  } console.log(matrix)
+
+  let arr = []
+  let x = 0 // index array
+  let y = 0 // index element
+  let top = param1
+  let bottom = 0
 
 
-    while(arr.length<param1*param1){
-      for (; y<top ; y++){
-        arr.push(matrix[x][y])
-      } 
-      x++
-      y--
-      for (; x<top ; x++){
-        arr.push(matrix[x][y])
-      }
-      y--
-      x--
-      for (;y >= bottom ; y--){
-        arr.push(matrix[x][y])
-      }
-      break
+  while (arr.length < param1 * param1) {
+    for (; y < top; y++) {
+      arr.push(matrix[x][y])
+    }
+    console.log(x, y)
 
-      
-    } console.log(arr)
+    x++
+    y--
+    for (; x < top; x++) {
+      arr.push(matrix[x][y])
+    }
+    console.log(x, y)
+
+    y--
+    x--
+    for (; y >= bottom; y--) {
+      arr.push(matrix[x][y])
+    }
+    console.log(x, y)
+
+    x--
+    y++
+    for (; x > 0; x--) {
+      arr.push(matrix[x][y])
+    }
+    console.log(x, y)
+    break
+
+
+  } console.log(arr)
 }
 
 
--- cek.js
+++ cek.js
@@ -1,4 +1,4 @@
-let param1 = 7
+let param1 = 5
 let matrix = []
 let count = 0
 
@@ -8,4 +8,24 @@ for (let i = 0; i < param1; i++) {
       matrix[i][j] = count;
       count++;
     }
-  }console.log(matrix[0][1])
+  }//console.log(matrix)
+
+  let arr = []
+  let x = 0 // index array
+  let y = 0 // index element
+  let top = param1
+  let bottom = 0
+
+  while(arr.length<param1*param1){
+    for (; y<top ; y++){
+      arr.push(matrix[x][y])
+    } 
+    console.log(x++)
+    console.log(y--)
+    for (; x<top ; x++){
+      arr.push(matrix[x][y])
+    }
+    console.log(y--)
+    console.log(x--)
+    break
+} //console.log(arr)
