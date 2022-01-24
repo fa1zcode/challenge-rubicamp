83c4dfd01babce9aa0427c7740b7f33e6f383986
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-24 16:42:21 +0700

    2 done

--- 2kaskuschallenge.js
+++ 2kaskuschallenge.js
@@ -1,14 +1,14 @@
 function deretKaskus(n) {
 
-    let hasil = []
+    let result = []                          // empty array to push result
 
-    for (let i = 3; i <= n * 3; i += 3) {
-        if (i % 5 === 0 && i % 6 === 0) { hasil.push('KASKUS') }
-        else if (i % 5 === 0) { hasil.push('KAS') }
-        else if (i % 6 === 0) { hasil.push('KUS') }
-        else hasil.push(i)
+    for (let i = 3; i <= n * 3; i += 3) {   // create series of n multiplied by 3
+        if (i % 5 === 0 && i % 6 === 0) { result.push('KASKUS') } // if divided by 5 and 6 equals 0 push KASKUS to result
+        else if (i % 5 === 0) { result.push('KAS') } // if can be divided by 5 push KAS
+        else if (i % 6 === 0) { result.push('KUS') } // if can be divided by 6 push KUS
+        else result.push(i) // if can't be divided by either 5 or 6 push the number
     }
-    return hasil
+    return result 
 }
 console.log(deretKaskus(10));
 
