e231e08047aa61e1fed24feb5f37cd9814ae9c54
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-26 10:15:17 +0700

    keep track how many times wrong

---
+++ 12tebaktebakanchlge.mjs
@@ -0,0 +1,50 @@
+import readline from 'readline';
+import fs from 'fs'
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tebakan: '
+});
+console.log('Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!')
+
+
+
+var soal = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+//console.log(soal)
+let index = 0
+console.log(`Pertanyaan: ${soal[index].definition}`)
+
+var counter = 0
+
+rl.prompt();
+
+rl.on('line', (answer) => {
+
+    //console.log(answer);
+    if (answer == soal[index].term) {
+        console.log('Selamat Anda Benar!')
+        index++
+        if (index < soal.length) {
+            console.log(`Pertanyaan: ${soal[index].definition}`)
+        }
+        else {
+            console.log('Hore Anda Menang!')
+            rl.close()
+        }
+    } else if (answer == 'skip'){
+        console.log(`Pertanyaan: ${soal[index++].definition}`)
+    }
+    
+    else {
+        counter++
+        console.log(`anda salah ${counter} kali`)
+
+    }
+
+    rl.prompt();
+
+}).on('close', () => {
+    process.exit(0);
+});
