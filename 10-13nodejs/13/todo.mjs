1c521be5c8786d8ef2abfd6760f5d4db33dbe68b
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-28 14:39:09 +0700

    can sort completed and uncompleted task

--- 10-13nodejs/13/todo.mjs
+++ 10-13nodejs/13/todo.mjs
@@ -3,8 +3,8 @@ let index = 0
 let param = process.argv
 let todo = JSON.parse(fs.readFileSync('todo.json', 'utf-8'))
 let arrLength = todo.length
-console.log(arrLength)
-console.log(todo)
+// console.log(arrLength)
+// console.log(todo)
 
 
 switch (param[2]) {
@@ -46,7 +46,44 @@ switch (param[2]) {
 
         fs.writeFileSync('todo.json', JSON.stringify(todo), 'utf-8')
         break;
-        
+
+    case 'outstanding':
+        if (param[3] == 'asc') {
+            todo.forEach((item, index) => {
+                if (todo[index].status == false) {
+                    console.log(`${index + 1}. ${item.status ? '[x]' : '[ ]'} ${item.tugas}`)
+                }
+            })
+        } else for (let i = todo.length - 1; i >= 0; i--) {
+            if (todo[i].status == false) {
+                // console.log(todo[i])
+                console.log(`${i + 1}. ${todo[i].status ? '[x]' : '[ ]'} ${todo[i].tugas}`)
+            }
+        }
+        break;
+
+    case 'completed':
+        if (param[3] == 'asc') {
+            todo.forEach((item, index) => {
+                if (todo[index].status == true) {
+                    console.log(`${index + 1}. ${item.status ? '[x]' : '[ ]'} ${item.tugas}`)
+                }
+            })
+        } else for (let i = todo.length - 1; i >= 0; i--) {
+            if (todo[i].status == true) {
+                // console.log(todo[i])
+                console.log(`${i + 1}. ${todo[i].status ? '[x]' : '[ ]'} ${todo[i].tugas}`)
+            }
+        }
+        break;
+
+    case 'tag':
+        let tag = param.slice(3, param.length)
+        console.log(tag)
+
+        // fs.writeFileSync('todo.json', JSON.stringify(todo), 'utf-8')
+        break;
+
     default:
         console.log('>>>JS TODO<<<')
         console.log('$ node todo.mjs <command>')
