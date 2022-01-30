fe37e2c0462ee14bf88a2ab6980fe7ab9b2d188e
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-01-30 19:55:10 +0700

    10 done

---
+++ 10-13nodejs/10readlinechallenge.mjs
@@ -0,0 +1,36 @@
+function sentenceManipulation(sentence) {
+
+    const vowel = ('AEIOUaeiou')
+    let kalimat = sentence.split(' ')
+    let berubahJadi = []
+
+
+    for (let i = 0; i < kalimat.length; i++) {
+        if (vowel.match((kalimat[i][0]))) {
+            berubahJadi.push(kalimat[i])
+        } else berubahJadi.push(kalimat[i].slice(1, kalimat[i].length)
+            + kalimat[i][0] + 'nyo')
+
+
+    } return berubahJadi.join(' ')
+}
+
+
+
+import readline from'readline';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'tulis kalimatmu disini > '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  console.log(`hasil konversi :${sentenceManipulation(line)}`);
+  
+  rl.prompt();
+}).on('close', () => {
+  console.log('Good bye!');
+  process.exit(0);
+});
