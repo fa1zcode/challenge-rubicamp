f18dd05b3cb07d8fb5e117e076edefeab7fe309d
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-02-04 11:09:30 +0700

    revise kontrak table

--- 14database/query.sql
+++ 14database/query.sql
@@ -1,4 +1,4 @@
-create table jurusan(
+CREATE TABLE jurusan(
     kodeJurusan varchar primary key not null,
     namaJurusan varchar
 );
@@ -16,7 +16,7 @@ insert into mahasiswa(nim, nama, alamat, jurusan)
 values('001', 'Faiz', 'Tangerang', 'J002');
 insert into mahasiswa(nim, nama, alamat, jurusan)
 values('002', 'Aad', 'Lampung', 'J002');
-create table matakuliah(
+CREATE TABLE matakuliah(
     kodeMataKuliah varchar primary key not null,
     nama varchar,
     sks varchar
@@ -24,37 +24,34 @@ create table matakuliah(
 insert into matakuliah(kodeMataKuliah, nama, sks)
 values('MK001', 'Aerodrome', '4'),
     ('MK002', 'Emergency Procedure', '2');
-CREATE TABLE kontrak (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nim varchar NOT NULL,
-    kodeMataKuliah varchar NOT NULL,
-    FOREIGN KEY (nim) REFERENCES mahasiswa (nim),
-    FOREIGN KEY (kodeMataKuliah) REFERENCES matakuliah (kodeMataKuliah)
+insert into kontrak(nim, kodeMataKuliah, nip)
+values('001', 'MK001', 'D001');
+insert into kontrak(nim, kodeMataKuliah, nip)
+values('001', 'MK002', 'D003');
+insert into kontrak(nim, kodeMataKuliah, nip)
+values('002', 'MK002', 'D002');
+insert into kontrak(nim, kodeMataKuliah, nip)
+values('002', 'MK001', 'D001');
+CREATE TABLE dosen(
+    nip varchar primary key not null,
+    nama varchar
 );
-insert into kontrak(nim, kodeMataKuliah)
-values('001', 'MK001');
-insert into kontrak(nim, kodeMataKuliah)
-values('001', 'MK002');
-insert into kontrak(nim, kodeMataKuliah)
-values('002', 'MK002');
-insert into kontrak(nim, kodeMataKuliah)
-values('002', 'MK001');
-
-
-create table dosen(nip varchar primary key not null, nama varchar);
 insert into dosen(nip, nama)
 values('D001', 'Djoko'),
     ('D002', 'Aminarno'),
     ('D003', 'Haryanto');
-CREATE TABLE mengajar (
+CREATE TABLE kontrak (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nip varchar NOT NULL,
+    nim varchar NOT NULL,
     kodeMataKuliah varchar NOT NULL,
-    FOREIGN KEY (nip) REFERENCES dosen (nip),
-    FOREIGN KEY (kodeMataKuliah) REFERENCES matakuliah (kodeMataKuliah)
+    nip varchar not null,
+    FOREIGN KEY (nim) REFERENCES mahasiswa (nim),
+    FOREIGN KEY (kodeMataKuliah) REFERENCES matakuliah (kodeMataKuliah) FOREIGN KEY (nip) REFERENCES dosen (nip)
 );
-insert into mengajar(nip, kodeMataKuliah)
-values('D001', 'MK001'),('D003', 'MK002'),('D002', 'MK001'),('D002', 'MK002');
-
-select mahasiswa.nama, kontrak.kodeMataKuliah, matakuliah.nama from mahasiswa join kontrak on mahasiswa.nim = kontrak.nim 
-join matakuliah on kontrak.kodeMataKuliah=matakuliah.kodeMataKuliah;
+select mahasiswa.nama,
+    matakuliah.nama,
+    dosen.nama
+from mahasiswa
+    join kontrak on mahasiswa.nim = kontrak.nim
+    join matakuliah on kontrak.kodeMataKuliah = matakuliah.kodeMataKuliah
+    join dosen on kontrak.nip = dosen.nip;
