c730aa10f14024f4e37265df7d295855267d121c
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-02-15 13:34:36 +0700

    delete + edir done

--- web/server.js
+++ web/server.js
@@ -39,6 +39,25 @@ app.post('/add', function(req, res){
     res.redirect('/')
 })
 
+app.get('/delete/:id', function(req, res){
+    const id = req.params.id
+    data.splice(id,1)
+    res.redirect('/')
+})
+
+app.get('/edit/:id', function(req,res){
+    const id = req.params.id
+    res.render('edit', {data: data[id]})
+})
+
+app.post('/edit/:id', function(req, res){
+    const id = req.params.id
+    data[id].task = req.body.task
+    data[id].complete = JSON.parse(req.body.complete)
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 3), 'utf-8')
+    res.redirect('/')
+})
+
 app.listen(3000, function(){
     console.log('web berjalan di port 3000')
 })
